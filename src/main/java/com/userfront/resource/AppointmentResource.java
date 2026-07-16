66f445eb7ac51822972d5226da2aec54d1a81ca8
Trí Phạm <111679271+tri22@users.noreply.github.com>
2026-07-17 01:10:43 +0700

    remove

--- src/main/java/com/userfront/resource/AppointmentResource.java
+++ src/main/java/com/userfront/resource/AppointmentResource.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.userfront.domain.Appointment;
 import com.userfront.service.AppointmentService;
-import com.userfront.dao.NewDao;
 
 @RestController
 @RequestMapping("/api/appointment")

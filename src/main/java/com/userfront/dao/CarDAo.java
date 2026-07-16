4a61fb408e60246fdd5d706b00d0b81c2c697ccf
Trí Phạm <111679271+tri22@users.noreply.github.com>
2026-07-16 22:15:40 +0700

    add group

---
+++ src/main/java/com/userfront/dao/CarDAo.java
@@ -0,0 +1,17 @@
+package com.userfront.dao;
+
+import java.util.List;
+
+public class CarDAo {
+    public String name;
+    public String model;
+    public String year;
+    public String color;
+    public String price;
+    public String description;
+    public String image;
+    public String status;
+    public String createdAt;
+    public String updatedAt;
+    public List<CarDAo> cars;
+}
